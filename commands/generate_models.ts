b8743ab109bbc6861dccad001e36c1f0985939bf
彭林 <penglin@xuejj.com>
2025-04-23 11:08:11 +0800

    指定表名称

--- commands/generate_models.ts
+++ commands/generate_models.ts
@@ -1,4 +1,4 @@
-import { BaseCommand } from '@adonisjs/core/ace'
+import { BaseCommand, args } from '@adonisjs/core/ace'
 import { CommandOptions } from '@adonisjs/core/types/ace'
 import Model from '../src/model/index.js'
 import { schema } from '../src/db/schema.js'
@@ -12,12 +12,23 @@ export default class GenerateModels extends BaseCommand {
     startApp: true,
   }
 
+  @args.string({ description: '表名称', required: false })
+  declare tableName: string
+
   async run() {
     const codemods = await this.createCodemods()
     const db = await this.app.container.make('lucid.db')
     const tables = await schema(db)
     const models = Model.build(tables)
 
+    if(this.tableName) {
+      for (let model of models) {
+        if(model.tableName === this.tableName) {
+          await codemods.makeUsingStub(stubsRoot, 'generate/model.stub', { model })
+        }
+      }
+      return
+    }
     for (let model of models) {
       await codemods.makeUsingStub(stubsRoot, 'generate/model.stub', { model })
     }
